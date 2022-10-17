f9e6e86221f25fc3a248be37797241d609ba173a
ssteinbach <ssteinbach@users.noreply.github.com>
2022-10-16 22:26:50 -0700

    use comptime t: type instead of anytype
    `comptime t:type` asserts that the argument is a type, so it'll be a
    compiler error if you try and pass something that ins't a type into the
    function.

--- src/json_reader.zig
+++ src/json_reader.zig
@@ -10,18 +10,14 @@ const c = @cImport(
     }
 );
 
-pub fn thingy(first: i32, second: f32) !first {
-    return first + @floatToInt(i32, second);
-}
-
-pub fn parse_json_err(file_path: []const u8, target_obj: anytype) !target_obj {
+pub fn parse_json_err(file_path: []const u8, comptime target_type: type) !target_type {
     const fi = try std.fs.cwd().openFile(file_path, .{});
     defer fi.close();
 
     const source = try fi.readToEndAlloc(ALLOCATOR, std.math.maxInt(u32));
     var stream = std.json.TokenStream.init(source);
 
-    return try std.json.parse(target_obj, &stream, .{});
+    return try std.json.parse(target_type, &stream, .{});
 }
 
 pub export fn parse_json_ptr(fpath:[*c]u8, result:*c.single_int) void {
@@ -43,8 +39,10 @@ pub export fn parse_json_array(fpath:[*c]u8) c.array_float {
 }
 
 test "test_parse" {
-    try expectEqual(parse_json_err("test.json"), 14);
-    try expectEqual(thingy(1, 2.3), @as(i32, 3));
+    try expectEqual(
+        parse_json_err("test.json", c.single_int),
+        c.single_int{.val=14}
+    );
 }
 
 pub export fn foo() i32 {
